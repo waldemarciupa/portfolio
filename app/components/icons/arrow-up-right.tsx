4cc91dce265376d6ba8b8e59f4b2516b717983f0
waldemarciupa <waldemarciupa90@gmail.com>
2024-12-20 20:19:17 +0100

    add arrow icon

---
+++ app/components/icons/arrow-up-right.tsx
@@ -0,0 +1,20 @@
+export default function ArrowUpRight(props) {
+  return (
+    <svg
+      width='1em'
+      height='1em'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        d='M7 17l10-10M7 7h10v10'
+        stroke='currentColor'
+        strokeWidth={2}
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+}
